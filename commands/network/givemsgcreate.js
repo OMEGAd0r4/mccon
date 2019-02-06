a9a062d02354b770aab001a24a66734464ced20a
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2019-02-06 09:43:23 +0700

    Create givemsgcreate.js

---
+++ commands/network/givemsgcreate.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+const commando = require('discord.js-commando');
+const prefix = ".";
+const bot = new commando.Client({
+  commandPrefix: prefix
+});
+
+class givemsgcreateCommand extends commando.Command {
+    constructor(client) 
+    {
+      super(client, {
+        name: 'givemsgcreate', 
+        group: 'network',
+        memberName: 'givemsgcreate',
+        description: "Broadcasts a message"
+      });
+    }
+
+    async run(message, args)
+    {
+        await message.delete();
+
+        var msgargs = message.content.slice(prefix.length).split(/ + /); //MAIN ARGS
+        var msgmsg = msgargs.join(" ").slice(14);
+        var staffrole = message.guild.roles.find(`name`, "Staff");
+
+        if (!msgmsg) return message.channel.send("Usage: .givemsgcreate <**message**>");
+
+        if (!message.member.roles.has(staffrole.id)) return message.channel.send("Insufficient permission");
+
+        var sendchannel = message.guild.channels.find(`name`, "giveaways");
+
+        sendchannel.send(msgmsg);
+    }
+}
+
+module.exports = givemsgcreateCommand;
