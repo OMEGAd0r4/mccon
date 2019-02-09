a4fca19d90f1a56b6cb729ff3dfa64016e6e8d51
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2019-02-09 15:55:18 +0700

    Create purge.js

---
+++ commands/network/purge.js
@@ -0,0 +1,16 @@
+        let messagecount = parseInt(args[1]) || 1;
+
+        var deletedMessages = -1;
+
+        message.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
+            messages.forEach(m => {
+                if (m.author.id == bot.user.id) {
+                    m.delete().catch(console.error);
+                    deletedMessages++;
+                }
+            });
+        }).then(() => {
+                if (deletedMessages === -1) deletedMessages = 0;
+                message.channel.send(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
+                    .then(m => m.delete(2000));
+        }).catch(console.error);
